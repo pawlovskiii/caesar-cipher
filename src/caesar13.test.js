0663eca851797db95978489eab764bdc0f68b4fb
Pawlovskiii <jakubpawwwlowski@gmail.com>
2021-06-17 18:28:41 +0200

    Initialized ESLint to the project

--- src/caesar13.test.js
+++ src/caesar13.test.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-undef */
 const caesar13 = require('./caesar13');
 
 test('should return the encrypted string shifted by 13 letters (lowerCase ONLY)', () => {
-	expect(caesar13('przeprogramowani')).toBe('cemrcebtenzbjnav');
-	expect(caesar13('blockchain')).toBe('oybpxpunva');
-	expect(caesar13('kryptografia')).toBe('xelcgbtensvn');
+  expect(caesar13('przeprogramowani')).toBe('cemrcebtenzbjnav');
+  expect(caesar13('blockchain')).toBe('oybpxpunva');
+  expect(caesar13('kryptografia')).toBe('xelcgbtensvn');
 });
