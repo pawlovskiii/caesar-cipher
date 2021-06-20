5fd0013ca4a8c497c808a604afed7c7f5dce8532
Pawlovskiii <jakubpawwwlowski@gmail.com>
2021-06-20 18:31:30 +0200

    Connected the algorithm with the page (displays top-left corner)

--- src/caesar13.js
+++ src/caesar13.js
@@ -1,38 +1,37 @@
+/* eslint-disable no-unused-vars */
 const upperCaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowerCaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
 
 function caesar13(input) {
-	if (typeof input !== 'string' || input === '') throw Error('Input must be a string and must not be empty.');
+	if (typeof input !== 'string' || input === '') {
+		throw Error('Input must be a string and must not be empty.');
+	}
 	const splittedInputToArray = [...input];
 	const shiftedOutputArray = [];
 	for (let i = 0; i < splittedInputToArray.length; i++) {
-		const indexLowerCaseAlphabetFromInput = lowerCaseAlphabet.indexOf(splittedInputToArray[i]);
-		const indexUpperCaseAlphabetFromInput = upperCaseAlphabet.indexOf(splittedInputToArray[i]);
+		const indexLowerCaseAlphabetFromInput = lowerCaseAlphabet.indexOf(
+			splittedInputToArray[i],
+		);
+		const indexUpperCaseAlphabetFromInput = upperCaseAlphabet.indexOf(
+			splittedInputToArray[i],
+		);
 		if (indexLowerCaseAlphabetFromInput !== -1) {
 			if (indexLowerCaseAlphabetFromInput + 13 < 26) {
 				shiftedOutputArray.push(
-					lowerCaseAlphabet.charAt(
-						indexLowerCaseAlphabetFromInput + 13,
-					),
+					lowerCaseAlphabet.charAt(indexLowerCaseAlphabetFromInput + 13),
 				);
 			}
 			shiftedOutputArray.push(
-				lowerCaseAlphabet.charAt(
-					indexLowerCaseAlphabetFromInput + 13 - 26,
-				),
+				lowerCaseAlphabet.charAt(indexLowerCaseAlphabetFromInput + 13 - 26),
 			);
 		} else if (indexUpperCaseAlphabetFromInput !== -1) {
 			if (indexUpperCaseAlphabetFromInput + 13 < 26) {
 				shiftedOutputArray.push(
-					upperCaseAlphabet.charAt(
-						indexUpperCaseAlphabetFromInput + 13,
-					),
+					upperCaseAlphabet.charAt(indexUpperCaseAlphabetFromInput + 13),
 				);
 			}
 			shiftedOutputArray.push(
-				upperCaseAlphabet.charAt(
-					indexUpperCaseAlphabetFromInput + 13 - 26,
-				),
+				upperCaseAlphabet.charAt(indexUpperCaseAlphabetFromInput + 13 - 26),
 			);
 		} else {
 			shiftedOutputArray.push(splittedInputToArray[i]);
@@ -43,3 +42,11 @@ function caesar13(input) {
 
 // module for testing
 module.exports = caesar13;
+
+// connecting to the HTML page
+function showEncryptedMessage() {
+	const inputText = document.querySelector('.message-place').value;
+	const displayOutputText = document.querySelector('.display-output-text');
+	console.log(inputText);
+	displayOutputText.textContent = caesar13(inputText);
+}
