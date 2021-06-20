78ef36caaec013a8a5b5590b7080645062617788
Pawlovskiii <jakubpawwwlowski@gmail.com>
2021-06-20 15:06:53 +0200

    Done code refactoring in the caesar13.js

--- src/caesar13.js
+++ src/caesar13.js
@@ -2,39 +2,43 @@ const upperCaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowerCaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
 
 function caesar13(input) {
-	if (typeof input !== 'string' || input === '') {
-		throw Error('Input must be a string and must not be empty.');
-	}
-	const splittedInput = [...input];
-	const shiftedInput = [];
-	for (let i = 0; i < splittedInput.length; i++) {
-		if (lowerCaseAlphabet.indexOf(splittedInput[i]) !== -1) {
-			if (lowerCaseAlphabet.indexOf(splittedInput[i]) + 13 < 26) {
-				shiftedInput.push(
+	if (typeof input !== 'string' || input === '') throw Error('Input must be a string and must not be empty.');
+	const splittedInputToArray = [...input];
+	const shiftedOutputArray = [];
+	for (let i = 0; i < splittedInputToArray.length; i++) {
+		const indexLowerCaseAlphabetFromInput = lowerCaseAlphabet.indexOf(splittedInputToArray[i]);
+		const indexUpperCaseAlphabetFromInput = upperCaseAlphabet.indexOf(splittedInputToArray[i]);
+		if (indexLowerCaseAlphabetFromInput !== -1) {
+			if (indexLowerCaseAlphabetFromInput + 13 < 26) {
+				shiftedOutputArray.push(
 					lowerCaseAlphabet.charAt(
-						lowerCaseAlphabet.indexOf(splittedInput[i]) + 13,
+						indexLowerCaseAlphabetFromInput + 13,
 					),
 				);
-			} else {
-				shiftedInput.push(lowerCaseAlphabet.charAt(
-					lowerCaseAlphabet.indexOf(splittedInput[i]) + 13 - 26,
-				));
 			}
-		} else if (upperCaseAlphabet.indexOf(splittedInput[i]) !== -1) {
-			if (upperCaseAlphabet.indexOf(splittedInput[i]) + 13 < 26) {
-				shiftedInput.push(
-					upperCaseAlphabet.charAt(upperCaseAlphabet.indexOf(splittedInput[i]) + 13),
+			shiftedOutputArray.push(
+				lowerCaseAlphabet.charAt(
+					indexLowerCaseAlphabetFromInput + 13 - 26,
+				),
+			);
+		} else if (indexUpperCaseAlphabetFromInput !== -1) {
+			if (indexUpperCaseAlphabetFromInput + 13 < 26) {
+				shiftedOutputArray.push(
+					upperCaseAlphabet.charAt(
+						indexUpperCaseAlphabetFromInput + 13,
+					),
 				);
-			} else {
-				shiftedInput.push(upperCaseAlphabet.charAt(
-					upperCaseAlphabet.indexOf(splittedInput[i]) + 13 - 26,
-				));
 			}
+			shiftedOutputArray.push(
+				upperCaseAlphabet.charAt(
+					indexUpperCaseAlphabetFromInput + 13 - 26,
+				),
+			);
 		} else {
-			shiftedInput.push(splittedInput[i]);
+			shiftedOutputArray.push(splittedInputToArray[i]);
 		}
 	}
-	return shiftedInput.join('');
+	return shiftedOutputArray.join('');
 }
 
 // module for testing
