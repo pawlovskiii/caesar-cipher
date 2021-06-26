4219688327277499bfe5afe27ebcefd1dc91ddeb
Pawlovskiii <jakubpawwwlowski@gmail.com>
2021-06-26 21:47:12 +0200

    New version of the main algorithm.

--- docs/scripts/caesar13.js
+++ docs/scripts/caesar13.js
@@ -1,43 +1,20 @@
-const upperCaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const lowerCaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-function caesar13(input) {
-	if (typeof input !== 'string' || input === '') {
-		throw Error('Input must be a string and must not be empty.');
-	}
+const caesar13 = (input) => {
+	if (typeof input !== 'string' || input === '') throw Error('Input must be a string and must not be empty.');
 	const ROTATION = 13;
-	const splittedInputToArray = [...input];
-	const shiftedOutputArray = [];
-	for (let i = 0; i < splittedInputToArray.length; i++) {
-		const indexLowerCaseAlphabetFromInput = lowerCaseAlphabet.indexOf(
-			splittedInputToArray[i],
-		);
-		const indexUpperCaseAlphabetFromInput = upperCaseAlphabet.indexOf(
-			splittedInputToArray[i],
-		);
-		if (indexLowerCaseAlphabetFromInput !== -1) {
-			if (indexLowerCaseAlphabetFromInput + ROTATION < 26) {
-				shiftedOutputArray.push(
-					lowerCaseAlphabet.charAt(indexLowerCaseAlphabetFromInput + ROTATION),
-				);
-			}
-			shiftedOutputArray.push(
-				lowerCaseAlphabet.charAt(indexLowerCaseAlphabetFromInput + ROTATION - 26),
-			);
-		} else if (indexUpperCaseAlphabetFromInput !== -1) {
-			if (indexUpperCaseAlphabetFromInput + ROTATION < 26) {
-				shiftedOutputArray.push(
-					upperCaseAlphabet.charAt(indexUpperCaseAlphabetFromInput + ROTATION),
-				);
-			}
-			shiftedOutputArray.push(
-				upperCaseAlphabet.charAt(indexUpperCaseAlphabetFromInput + ROTATION - 26),
-			);
-		} else {
-			shiftedOutputArray.push(splittedInputToArray[i]);
-		}
-	}
-	return shiftedOutputArray.join('');
-}
+
+	const splittedInput = [...input];
+	const encryptedResult = splittedInput.map((item) => {
+		const charIndexInAlphabet = alphabet.indexOf(item.toLowerCase());
+		if (charIndexInAlphabet === -1) return item;
+		const newCharPosition = (charIndexInAlphabet + ROTATION) % 26;
+
+		return item === item.toUpperCase()
+			? alphabet.charAt(newCharPosition).toUpperCase()
+			: alphabet.charAt(newCharPosition);
+	});
+	return encryptedResult.join('');
+};
 
 export { caesar13 };
