b9b69877acac7a52d5a081c914932e6126794958
Pawlovskiii <jakubpawwwlowski@gmail.com>
2021-07-04 12:46:36 +0200

    Refactored the caesar13 function

--- docs/scripts/caesar13.js
+++ docs/scripts/caesar13.js
@@ -4,15 +4,13 @@ const caesar13 = (input) => {
 	if (typeof input !== 'string' || input === '') throw Error('Input must be a string and must not be empty.');
 	const ROTATION = 13;
 
-	const splittedInput = [...input];
-	const encryptedResult = splittedInput.map((item) => {
+	const encryptedResult = [...input].map((item) => {
 		const charIndexInAlphabet = alphabet.indexOf(item.toLowerCase());
 		if (charIndexInAlphabet === -1) return item;
 		const newCharPosition = (charIndexInAlphabet + ROTATION) % 26;
-
-		return item === item.toUpperCase()
-			? alphabet.charAt(newCharPosition).toUpperCase()
-			: alphabet.charAt(newCharPosition);
+		return item !== item.toUpperCase()
+			? alphabet.charAt(newCharPosition)
+			: alphabet.charAt(newCharPosition).toUpperCase();
 	});
 	return encryptedResult.join('');
 };
