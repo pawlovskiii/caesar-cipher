c306295c240a000e165d0b02e0fab9dcd9449f55
Pawlovskiii <jakubpawwwlowski@gmail.com>
2021-07-14 19:10:19 +0200

    Added key - second parameter in the caesar13 function

--- docs/__tests__/caesar13.test.js
+++ docs/__tests__/caesar13.test.js
@@ -14,14 +14,14 @@ describe('Initial input validation', () => {
 
 describe('Lower case only - latin letters', () => {
 	test('should return shifted (ROT13) expression', () => {
-		expect(caesar13('gas price ethereum')).toBe('tnf cevpr rgurerhz');
-		expect(caesar13('proof of work')).toBe('cebbs bs jbex');
+		expect(caesar13('gas price ethereum', 13)).toBe('tnf cevpr rgurerhz');
+		expect(caesar13('proof of work', 13)).toBe('cebbs bs jbex');
 	});
 });
 
 describe('Upper case only - latin letters', () => {
 	test('should return shifted (ROT13) expression', () => {
-		expect(caesar13('DECENTRALIZED APPLICATION')).toBe(
+		expect(caesar13('DECENTRALIZED APPLICATION', 13)).toBe(
 			'QRPRAGENYVMRQ NCCYVPNGVBA'
 		);
 	});
@@ -29,13 +29,13 @@ describe('Upper case only - latin letters', () => {
 
 describe('Mix of lowerCased and upperCased - latin letters', () => {
 	test('should return shifted (ROT13) expression', () => {
-		expect(caesar13('Encrypt THis MeSSage')).toBe('Rapelcg GUvf ZrFFntr');
+		expect(caesar13('Encrypt THis MeSSage', 13)).toBe('Rapelcg GUvf ZrFFntr');
 	});
 });
 
 describe('Mix of latin letters and digits/special characters - {lowerCased only}', () => {
 	test('should return shifted (ROT13) expression', () => {
-		expect(caesar13('1test 2driven 3development !!!')).toBe(
+		expect(caesar13('1test 2driven 3development !!!', 13)).toBe(
 			'1grfg 2qevira 3qrirybczrag !!!'
 		);
 	});
@@ -43,12 +43,12 @@ describe('Mix of latin letters and digits/special characters - {lowerCased only}
 
 describe('Mix of latin letters and digits/special characters - {upperCased only}', () => {
 	test('should return shifted (ROT13) expression', () => {
-		expect(caesar13('CRYPTO 999 CORP @#$ !@')).toBe('PELCGB 999 PBEC @#$ !@');
+		expect(caesar13('CRYPTO 999 CORP @#$ !@', 13)).toBe('PELCGB 999 PBEC @#$ !@');
 	});
 });
 
 describe('Mix of latin letters and digits/special characters - {lowerCased and upperCased}', () => {
 	test('should return shifted (ROT13) expression', () => {
-		expect(caesar13('JeSt is fun to DO! !!@11')).toBe('WrFg vf sha gb QB! !!@11');
+		expect(caesar13('JeSt is fun to DO! !!@11', 13)).toBe('WrFg vf sha gb QB! !!@11');
 	});
 });
--- docs/scripts/caesar13.js
+++ docs/scripts/caesar13.js
@@ -1,18 +1,17 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-const caesar13 = (input) => {
-	if (typeof input !== 'string' || input === '') throw Error('Invalid input.');
-	const ROTATION = 13;
+function caesar13(string, key) {
+	if (typeof string !== 'string' || string === '') throw Error('Invalid input.');
 
-	const encryptedResult = [...input].map((item) => {
+	const encryptedResult = [...string].map((item) => {
 		const charIndexInAlphabet = alphabet.indexOf(item.toLowerCase());
 		if (charIndexInAlphabet === -1) return item;
-		const newCharPosition = (charIndexInAlphabet + ROTATION) % 26;
+		const newCharPosition = (charIndexInAlphabet + key) % 26;
 		return item !== item.toUpperCase()
 			? alphabet.charAt(newCharPosition)
 			: alphabet.charAt(newCharPosition).toUpperCase();
 	});
 	return encryptedResult.join('');
-};
+}
 
 export { caesar13 };
