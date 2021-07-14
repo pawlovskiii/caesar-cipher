72f3be63292a0dbd578201555de3237613e8d996
Pawlovskiii <jakubpawwwlowski@gmail.com>
2021-07-14 20:17:32 +0200

    Updated test cases for specifying the key-length shift

--- docs/__tests__/caesar13.test.js
+++ docs/__tests__/caesar13.test.js
@@ -13,42 +13,42 @@ describe('Initial input validation', () => {
 });
 
 describe('Lower case only - latin letters', () => {
-	test('should return shifted (ROT13) expression', () => {
-		expect(caesar13('gas price ethereum', 13)).toBe('tnf cevpr rgurerhz');
-		expect(caesar13('proof of work', 13)).toBe('cebbs bs jbex');
+	test('should return key-length shifted expression', () => {
+		expect(caesar13('gas price ethereum', 15)).toBe('vph egxrt tiwtgtjb');
+		expect(caesar13('proof of work', 4)).toBe('tvssj sj asvo');
 	});
 });
 
 describe('Upper case only - latin letters', () => {
-	test('should return shifted (ROT13) expression', () => {
-		expect(caesar13('DECENTRALIZED APPLICATION', 13)).toBe(
-			'QRPRAGENYVMRQ NCCYVPNGVBA'
+	test('should return key-length shifted expression', () => {
+		expect(caesar13('DECENTRALIZED APPLICATION', 2)).toBe(
+			'FGEGPVTCNKBGF CRRNKECVKQP'
 		);
 	});
 });
 
 describe('Mix of lowerCased and upperCased - latin letters', () => {
-	test('should return shifted (ROT13) expression', () => {
-		expect(caesar13('Encrypt THis MeSSage', 13)).toBe('Rapelcg GUvf ZrFFntr');
+	test('should return key-length shifted expression', () => {
+		expect(caesar13('Encrypt THis MeSSage', 9)).toBe('Nwlahyc CQrb VnBBjpn');
 	});
 });
 
 describe('Mix of latin letters and digits/special characters - {lowerCased only}', () => {
-	test('should return shifted (ROT13) expression', () => {
-		expect(caesar13('1test 2driven 3development !!!', 13)).toBe(
-			'1grfg 2qevira 3qrirybczrag !!!'
+	test('should return key-length shifted expression', () => {
+		expect(caesar13('1test 2driven 3development !!!', 22)).toBe(
+			'1paop 2zneraj 3zarahkliajp !!!'
 		);
 	});
 });
 
 describe('Mix of latin letters and digits/special characters - {upperCased only}', () => {
-	test('should return shifted (ROT13) expression', () => {
+	test('should return key-length shifted expression', () => {
 		expect(caesar13('CRYPTO 999 CORP @#$ !@', 13)).toBe('PELCGB 999 PBEC @#$ !@');
 	});
 });
 
 describe('Mix of latin letters and digits/special characters - {lowerCased and upperCased}', () => {
-	test('should return shifted (ROT13) expression', () => {
-		expect(caesar13('JeSt is fun to DO! !!@11', 13)).toBe('WrFg vf sha gb QB! !!@11');
+	test('should return key-length shifted expression', () => {
+		expect(caesar13('JeSt is fun to DO! !!@11', 20)).toBe('DyMn cm zoh ni XI! !!@11');
 	});
 });
