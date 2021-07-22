d0e1e68ff20b57a8b1da6c674f2782d5c1af0b1a
Pawlovskiii <jakubpawwwlowski@gmail.com>
2021-07-22 14:24:58 +0200

    Added again - reset button and refactored the code

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,18 +1,18 @@
 import { caesar13 } from './caesar13.js';
 
-const showEncryptedMessage = () => {
-	const inputText = document.querySelector('.input-message-text').value;
-	const key = document.querySelector('.key-holder-input').value;
-	const displayOutputText = document.querySelector('.display-output-text');
-	if (inputText.match(/[a-zA-Z]/g)) {
-		displayOutputText.innerText = caesar13(inputText, Number(key));
+const displayOutputText = document.querySelector('.display-output-text');
+const btnEncrypt = document.querySelector('#btn-encrypt');
+const btnReset = document.querySelector('#btn-reset');
+
+function showEncryptedMessage() {
+	const inputTextMessage = document.querySelector('.input-message-text').value;
+	const keyShift = document.querySelector('.key-holder-input').value;
+	if (inputTextMessage.match(/[a-zA-Z]/g)) {
+		displayOutputText.innerText = caesar13(inputTextMessage, Number(keyShift));
 	} else {
 		displayOutputText.innerText = 'Invalid input.';
 	}
 };
-
-const btnEncrypt = document.querySelector('#btn-encrypt');
-btnEncrypt.addEventListener('click', showEncryptedMessage);
 
 const resetDisplayedText = () => {
 	document.querySelector('.input-message-text').value = '';
@@ -20,3 +20,6 @@ const resetDisplayedText = () => {
 	document.querySelector('.display-output-text').textContent = '';
 	document.querySelector('.key-holder-input').value = 0;
 };
+
+btnEncrypt.addEventListener('click', showEncryptedMessage);
+btnReset.addEventListener('click', resetDisplayedText);
