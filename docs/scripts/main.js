0e52bc10c5248d87095c26cf4189410827789c0f
Pawlovskiii <jakubpawwwlowski@gmail.com>
2021-07-22 14:06:13 +0200

    Applied Flexbox to the project, refactored whole CSS and HTML file

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,8 +1,8 @@
 import { caesar13 } from './caesar13.js';
 
 const showEncryptedMessage = () => {
-	const inputText = document.querySelector('.message-place').value;
-	const key = document.querySelector('.key-holder').value;
+	const inputText = document.querySelector('.input-message-text').value;
+	const key = document.querySelector('.key-holder-input').value;
 	const displayOutputText = document.querySelector('.display-output-text');
 	if (inputText.match(/[a-zA-Z]/g)) {
 		displayOutputText.innerText = caesar13(inputText, Number(key));
@@ -15,8 +15,8 @@ const btnEncrypt = document.querySelector('#btn-encrypt');
 btnEncrypt.addEventListener('click', showEncryptedMessage);
 
 const resetDisplayedText = () => {
-	document.querySelector('.message-place').value = '';
+	document.querySelector('.input-message-text').value = '';
 	document.querySelector('.display-output-text').value = '';
 	document.querySelector('.display-output-text').textContent = '';
-	document.querySelector('.key-holder').value = 0;
+	document.querySelector('.key-holder-input').value = 0;
 };
